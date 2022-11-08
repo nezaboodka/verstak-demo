00c65b6efdb07244813765ef6199d28730aba51a
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-08 09:22:32 +0300

    Added the concept of presets

---
+++ source/views/Main.p.ts
@@ -0,0 +1,18 @@
+import { Render } from "verstak"
+
+export const Content: Render<HTMLElement> = e => {
+  e.style.margin = "1rem"
+  e.style.border = "1px solid blue"
+}
+
+export const Important: Render<HTMLElement> = e => {
+  e.style.color = "red"
+  e.style.backgroundColor = "rgba(255, 0, 0, 0.1)"
+  e.style.border = "1px solid red"
+}
+
+export const Unimportant: Render<HTMLElement> = e => {
+  e.style.color = "silver"
+  e.style.fontSize = "smaller"
+  e.style.border = "1px solid silver"
+}
--- source/views/Main.v.ts
+++ source/views/Main.v.ts
@@ -1,6 +1,7 @@
-import { block, BlockOptions, br } from "verstak"
+import { block, BlockOptions, br, P, text } from "verstak"
 import { App } from "models/App"
 import { Panel } from "./Panel.v"
+import * as z from "./Main.p"
 
 export function Main(name: string, app: App, options?: BlockOptions<HTMLElement, void, void>) {
   return (
@@ -12,9 +13,16 @@ export function Main(name: string, app: App, options?: BlockOptions<HTMLElement,
       }
 
       br()
-      Panel("Logo", "...")
-      Panel("Toolbar", "...", { box: { widthGrow: 1 } })
-      Panel("Account", "...", { box: { widthGrow: 1 } })
+      block("logo", { as: [z.Content, z.Important] }, e => {
+        text("Logo")
+      })
+      block("toolbar", { as: [z.Content], box: { widthGrow: 1 } }, e => {
+        text("Toolbar")
+        br(); text("Toolbar can be multi-line")
+      })
+      block("account", { as: [z.Content, z.Unimportant] } , e => {
+        text("Account")
+      })
 
       br()
       Panel("Navigation-Bar", "...")
