6f831f6a5e55411a42c699c2ec1987b5dc8fea1e
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-08 12:52:04 +0300

    Mixins

--- source/views/Panel.v.ts
+++ source/views/Panel.v.ts
@@ -4,7 +4,7 @@ import * as css from "theme/Common.css"
 export function Panel(name: string,
   preset?: BlockPreset<HTMLElement, void, void>,
   render?: Render<HTMLElement, void, void>) {
-  preset = presetsToOptions(preset, { as: [css.Panel], wrapper: render })
+  preset = presetsToOptions(preset, { mixins: [css.Panel], wrapper: render })
   return (
     Block(name, preset, (e, b) => {
       $(name)
