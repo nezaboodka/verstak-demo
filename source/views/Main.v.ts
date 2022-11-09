7497c68913645810e57026f9218a14bc84047821
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-09 08:31:29 +0300

    Cosmetics

--- source/views/Main.v.ts
+++ source/views/Main.v.ts
@@ -9,44 +9,46 @@ export function Main(name: string, app: App) {
     Block(name, {
       reactor: true,
       render(e, b) {
-        $row(s => {
-          ToolBar("ToolBar", {
-            widthGrow: 1,
-          })
+        $rowBegin()
+
+        ToolBar("ToolBar", {
+          widthGrow: 1,
         })
-        $row(s => {
-          Block("NavBar", {
-            reuse: [css.Panel],
-            widthMin: "10rem",
-            align: Align.TopCenter,
-            render() {
-              $`Navigation Bar`
-            }
-          })
-          Block("WorkArea", {
-            reuse: [css.Panel, css.Important],
-            widthGrow: 1,
-            heightGrow: 1,
-            align: Align.MiddleCenter,
-            render() {
-              $html`Hello, <b>Verstak</b>!<br/><b>How</b> are you doing today?`
-            }
-          })
-          Block("PropInspector", {
-            reuse: [css.Panel],
-            widthMin: "15rem", widthMax: "15rem",
-            align: Align.FitButTop,
-            render() {
-              $html`СПРАВКА`
-              $rowBegin()
-              $html`<br/><b>Верстак</b> – это система построения визуальных интерфейсов на основе табличной вёрстки. В основе лежит идея размещения визуальных элементов интерфейса в одиночных или смежных ячейках таблицы. Размещаемые элементы называются блоками. При этом сама таблица с размещёнными внутри неё блоками также считается блоком и в свою очередь может быть размещена в другом блоке уже на его табличной сетке.`
-            }
-          })
+
+        $rowBegin()
+
+        Block("NavBar", {
+          reuse: [css.Panel],
+          widthMin: "10rem",
+          align: Align.TopCenter,
+          render() {
+            $`Navigation Bar`
+          }
         })
-        $row(() => {
-          StatusBar("StatusBar", {
-            widthGrow: 1,
-          })
+        Block("WorkArea", {
+          reuse: [css.Panel, css.Important],
+          widthGrow: 1,
+          heightGrow: 1,
+          align: Align.MiddleCenter,
+          render() {
+            $html`<b>Verstak</b> is a front-end library<br/>for building interactive user interfaces`
+          }
+        })
+        Block("PropInspector", {
+          reuse: [css.Panel],
+          widthMin: "15rem", widthMax: "15rem",
+          align: Align.FitButTop,
+          render() {
+            $html`СПРАВКА`
+            $rowBegin()
+            $html`<br/><b>Верстак</b> – это система построения визуальных интерфейсов на основе табличной вёрстки. В основе лежит идея размещения визуальных элементов интерфейса в одиночных или смежных ячейках таблицы. Размещаемые элементы называются блоками. При этом сама таблица с размещёнными внутри неё блоками также считается блоком и в свою очередь может быть размещена в другом блоке уже на его табличной сетке.`
+          }
+        })
+
+        $rowBegin()
+
+        StatusBar("StatusBar", {
+          widthGrow: 1,
         })
       },
     })
