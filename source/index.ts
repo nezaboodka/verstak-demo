d267ede234c1d9d367c7482ffcd4b2bd0ecdf05e
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-12 00:00:16 +0300

    Code structure

--- source/index.ts
+++ source/index.ts
@@ -4,12 +4,12 @@ import { configureDebugging } from "dbg"
 import { App } from "models/App"
 import { Main } from "views/Main.v"
 
-import "../public/assets/reset.css"
+import "../index.reset.css"
 import "../public/assets/verstak.css"
-import "../public/assets/index.css"
-import "../public/assets/code.light.css"
-import "../public/assets/code.dark.css"
-import "../public/assets/code.print.css"
+import "../index.css"
+import "theme/code.light.css"
+import "theme/code.dark.css"
+import "theme/code.print.css"
 
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 const version: string = "0.1"
