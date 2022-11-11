8c4824a322ca67a14186e1aba2446d5300249bf9
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-11 07:49:27 +0300

    Example

--- source/views/WorkArea.v.ts
+++ source/views/WorkArea.v.ts
@@ -5,41 +5,23 @@ export function WorkArea(name: string,
   args?: Partial<BlockArgs<HTMLElement, void, void>>) {
   return (
     Grid(name, { ...args, wrapper: args?.render, render(e, b) {
-
-      // Grid Flow: rightwards-downwards
+      // When "place" arg is not provided, then cells are
+      // assigned to blocks sequentially rightwards-downwards
+      // with the respect to line feeds.
       Ruler("1", To.Left, true)
       Ruler("A", To.Top)
       Ruler("B", To.Top)
       Ruler("C", To.Top); lineFeed()
       Ruler("2", To.Left); lineFeed()
       Ruler("3", To.Left); lineFeed()
-
-      // Absolute positioning inside grid
-      Block("B2", {
-        mixins: [m.Important],
-        place: "B2",
-        render(e, b) { Plain("B2") }
-      })
-      Block("A1:B1", {
-        mixins: [m.Important],
-        place: "A1:B1",
-        render(e, b) { Plain("A1:B1") }
-      })
-      Block("C1:C2", {
-        mixins: [m.Important],
-        place: "C1:C2",
-        render(e, b) { Plain("C1:C2") }
-      })
-      Block("B3:C3", {
-        mixins: [m.Important],
-        place: "B3:C3",
-        render(e, b) { Plain("B3:C3") }
-      })
-      Block("A2:A3", {
-        mixins: [m.Important],
-        place: "A2:A3",
-        render(e, b) { Plain("A2:A3") }
-      })
+      // When "place" arg is provided, then blocks are
+      // positioned exactly at the provided cell or
+      // cell range address.
+      ExampleData("B2")
+      ExampleData("A1:B1")
+      ExampleData("C1:C2")
+      ExampleData("B3:C3")
+      ExampleData("A2:A3")
     }})
   )
 }
@@ -51,6 +33,16 @@ function Ruler(title: string, dock: To, overlap?: boolean) {
     render(e) {
       e.style.zIndex = "1"
       Markdown(`&nbsp;${title}`)
+    }
+  })
+}
+
+function ExampleData(place: string) {
+  Block(place, {
+    mixins: [m.Important],
+    place, // absolute position inside grid
+    render(e, b) {
+      Plain(place)
     }
   })
 }
