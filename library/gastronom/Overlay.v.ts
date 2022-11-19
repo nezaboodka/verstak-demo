825d7b88acbdd4bb47a11933ad981341e77a65a9
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-19 22:57:31 +0300

    Cleanup

--- library/gastronom/Overlay.v.ts
+++ library/gastronom/Overlay.v.ts
@@ -12,19 +12,6 @@ export const Overlay = (body?: BlockBody<HTMLElement, OverlayModel>) => (
       b.overlay = true
       e.style.outlineOffset = "-0.5px"
       e.style.backgroundColor = "white"
-      const parent = b.host.native
-      if (parent instanceof HTMLElement) {
-        const bounds = parent.getBoundingClientRect()
-        const x = document.body.offsetWidth - bounds.left
-        if (x < document.body.offsetWidth / 2)
-          e.style.right = `${document.body.offsetWidth - bounds.right}px`
-        else
-          e.style.left = `${bounds.left}px`
-        if (bounds.top > document.body.clientHeight / 2)
-          e.style.bottom = `${document.body.offsetHeight - bounds.top + 1}px`
-        else
-          e.style.top = `${bounds.bottom + 1}px`
-      }
     },
     render(b) {
       const e = b.native
