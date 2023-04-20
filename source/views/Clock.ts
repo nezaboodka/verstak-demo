99dfe027a55d76a5174861e9176bc21480cb7d1c
Yury Chetyrko <ychetyrko@gmail.com>
2023-04-20 14:02:53 +0300

    Watch

--- source/views/Clock.ts
+++ source/views/Clock.ts
@@ -32,7 +32,7 @@ export function Clock(area: string): Block<HTMLElement> {
                 e.height.baseVal.value = 1000
                 e.rx.baseVal.value = 5
                 e.ry.baseVal.value = 5
-                e.style.stroke = "black"
+                e.style.stroke = "gray"
                 e.style.fill = "silver"
                 e.style.strokeWidth = "4px"
               },
@@ -46,7 +46,7 @@ export function Clock(area: string): Block<HTMLElement> {
                 e.height.baseVal.value = 120
                 e.rx.baseVal.value = 20
                 e.ry.baseVal.value = 20
-                e.style.stroke = "black"
+                e.style.stroke = "gray"
                 e.style.fill = "silver"
                 e.style.strokeWidth = "4px"
                 rotate(e, 32)
@@ -58,7 +58,7 @@ export function Clock(area: string): Block<HTMLElement> {
                 e.cx.baseVal.value = 500
                 e.cy.baseVal.value = 500
                 e.r.baseVal.value = 490
-                e.style.stroke = "black"
+                e.style.stroke = "gray"
                 e.style.fill = "silver"
                 e.style.strokeWidth = "3px"
               },
@@ -69,34 +69,35 @@ export function Clock(area: string): Block<HTMLElement> {
                 e.cx.baseVal.value = 500
                 e.cy.baseVal.value = 500
                 e.r.baseVal.value = 400
-                e.style.stroke = "silver"
+                e.style.stroke = "gray"
                 e.style.fill = "#1a3043"
-                e.style.strokeWidth = "10px"
+                e.style.strokeWidth = "3px"
               },
             })
-            Circle({
-              render(b) {
-                const e = b.native
-                e.cx.baseVal.value = 500
-                e.cy.baseVal.value = 207
-                e.r.baseVal.value = 45
-                e.style.stroke = "rgba(0, 0, 0, 0.25)"
-                e.style.fill = "#93CAEC"
-                e.style.strokeWidth = "2px"
-              },
-            })
+            // Circle({
+            //   render(b) {
+            //     const e = b.native
+            //     e.cx.baseVal.value = 500
+            //     e.cy.baseVal.value = 196
+            //     e.r.baseVal.value = 45
+            //     e.style.stroke = "rgba(0, 0, 0, 0.25)"
+            //     e.style.fill = "#93CAEC"
+            //     e.style.strokeWidth = "2px"
+            //   },
+            // })
 
-            radialDots("#AAAAAA", 15, 30, 270)
-            radialDots("#AAAAAA", 6, 0, 105)
+            radialDots("#AAAAAA", 15, 0, 270)
+            radialDots("#AAAAAA", 6, 30, 105)
+            radialDots("white", 45, 45, 270)
             //dots("magenta", 15, 30, 0)
 
             // Hours & Minutes
-            radialLabels(svg, 290, 100, "bold", "white", false,
-              [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22], 15, 180)
-            // radialLabels(svg, 250, 35, "normal", "#CCCCCC", false,
-            //   [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23], 15, 180)
-            radialLabels(svg, 370, 40, "normal", "#CCCCCC", true,
-              [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55], 6, 0)
+            radialLabels(svg, 300, 100, "bold", "white", false,
+              [0, 3, 6, 9, 12, 15, 18, 21], 15, 180)
+            radialLabels(svg, 255, 40, "normal", "#CCCCCC", false,
+              [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23], 15, 180)
+            radialLabels(svg, 365, 40, "normal", "#CCCCCC", true,
+              [0, 10, 20, 30, 40, 50], 6, 0)
 
             // Arrows
             Arrow(30, 0.425, 5, 60, 60 * 60 * 24, "white", "black")
@@ -132,19 +133,23 @@ export function Clock(area: string): Block<HTMLElement> {
                   rotate(b.native, 0)
                 radialDots("#555555", 15, 90, 87)
                 // dots(svg, -98)
-                radialLabels(svg, 445, 50, "normal", "#111111", true,
-                  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], 15, 180)
-                Circle({
-                  render(b) {
-                    const e = b.native
-                    e.cx.baseVal.value = 500
-                    e.cy.baseVal.value = 55
-                    e.r.baseVal.value = 30
-                    e.style.stroke = "black"
-                    e.style.fill = "#93CAEC"
-                    e.style.strokeWidth = "2px"
-                  },
-                })
+                radialLabels(svg, 445, 70, "bold", "#111111", true,
+                  [0, 12], 15, 180)
+                radialLabels(svg, 445, 70, "normal", "#111111", true,
+                  [3, 6, 9, 15, 18, 21], 15, 180)
+                radialLabels(svg, 435, 40, "normal", "#111111", true,
+                  [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23], 15, 180)
+                // Circle({
+                //   render(b) {
+                //     const e = b.native
+                //     e.cx.baseVal.value = 500
+                //     e.cy.baseVal.value = 55
+                //     e.r.baseVal.value = 30
+                //     e.style.stroke = "black"
+                //     e.style.fill = "#93CAEC"
+                //     e.style.strokeWidth = "2px"
+                //   },
+                // })
               },
             })
           },
@@ -286,8 +291,9 @@ function radialLabels(root: SVGSVGElement,
         content = content[0] + " " + content[1]
     }
     RadialLabel(n * step + basis, content,
-      content !== "☀" ? color : "#FFFFB7",
+      color, // content !== "☀" ? color : "#FFFFB7",
       radius, size,
-      n % 12 !== 0 ? weight : "normal", bezel, root)
+      weight, // !bezel && n % 12 !== 0 ? weight : "normal",
+      bezel, root)
   }
 }
