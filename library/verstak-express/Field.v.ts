d0c1d4fcfe8977ba17c0c6957ca51e23628df154
Yury Chetyrko <ychetyrko@gmail.com>
2024-02-11 11:01:41 +0300

    Introduced the concept of Range

--- library/verstak-express/Field.v.ts
+++ library/verstak-express/Field.v.ts
@@ -64,7 +64,7 @@ function FieldInput(model: FieldModel, s: FieldStyling) {
       initialize(b, base) {
         const e = b.native
         b.useStylingPreset(s.input)
-        b.widthGrowth = 1
+        b.widthRange = { growth: 1 }
         e.tabIndex = 0
         e.contentEditable = "true"
         e.dataForSensor.focus = model
--- source/views/StatusBar.v.ts
+++ source/views/StatusBar.v.ts
@@ -69,12 +69,12 @@ export function StatusBar(declaration?: RxNodeDecl<El<HTMLElement, void>>) {
         Section({
           update(b) {
             b.useStylingPreset(theme.panel)
-            b.widthGrowth = 1
+            b.widthRange = { growth: 1 }
             b.contentAlignment = Align.right
             Field({
               initialize(b, base) {
                 const loader = app.loader
-                b.minWidth = "10em"
+                b.width = "10em"
                 b.model = composeFieldModel({
                   icon: "fa-solid fa-search",
                   text: refs(loader).filter,
