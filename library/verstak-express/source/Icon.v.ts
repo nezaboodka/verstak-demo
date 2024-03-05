208b968dea58be938af8c969c9e5935821ed3d34
Vadim Seledets <vadim.seledets@nezaboodka.com>
2024-03-05 15:08:25 +0300

    Re-render "Icon" when its name changes

--- library/verstak-express/source/Icon.v.ts
+++ library/verstak-express/source/Icon.v.ts
@@ -14,6 +14,7 @@ export function Icon(name: string,
   return (
     Section(declaration, {
       mode: Mode.independentUpdate,
+      triggers: { name },
       onChange: el => {
         const theme = Theme.current.icon
         el.useStylingPreset(name)
