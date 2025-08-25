eff2d8b65d4900d64e3a3a3d181783beb95f4d0b
Yury Chetyrko <yury.chetyrko@nezaboodka.com>
2025-08-25 09:56:29 +0300

    R R 0.94.25032

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
-import { Mode } from "reactronic"
+import { launch, Mode } from "reactronic"
 import { RealTimeClock, WebDriver, Window, rowBreak } from "verstak"
 import { configureDebugging } from "dbg.js"
 import { DemoApp } from "models/DemoApp.js"
@@ -16,26 +16,26 @@ const version: string = "0.1"
 
 configureDebugging()
 
-const win = Window({
-  mode: Mode.autonomous,
-  preparation: el => {
-    DemoApp.current = new DemoApp(version,
-      new LightAppTheme(),
-      new DarkAppTheme(),
-      new PrintAppTheme())
-  },
-  script: el => {
-    const clock = new RealTimeClock(200)
-    const theme = DemoApp.current.theme
-    const s = el.style
-    s.color = theme.textColor
-    s.backgroundColor = theme.spaceFillColor
-    el.useStylingPreset(theme.page)
-    rowBreak() // WORKAROUND
-    MainWindow(clock)
-  }
-})
+const win = launch(
+  Window({
+    mode: Mode.autonomous,
+    preparation: el => {
+      DemoApp.current = new DemoApp(version,
+        new LightAppTheme(),
+        new DarkAppTheme(),
+        new PrintAppTheme())
+    },
+    script: el => {
+      const clock = new RealTimeClock(200)
+      const theme = DemoApp.current.theme
+      const s = el.style
+      s.color = theme.textColor
+      s.backgroundColor = theme.spaceFillColor
+      el.useStylingPreset(theme.page)
+      rowBreak() // WORKAROUND
+      MainWindow(clock)
+    }
+  }))
 
 console.log(WebDriver.getOwnNodeOfNativeElement(win.element.native))
-
-console.log(win.lookupTreeNodeByUri("node://·A·Window@index.ts:16:13/·B·MainWindow@index.ts:34:5/Theme"))
+console.log(win.lookupTreeNodeByUri("node://·A·Window@index.ts:17:3/·B·MainWindow@index.ts:34:5/Theme"))
